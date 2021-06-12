7fbeff8fe8fb55faf276766c49f0defd05cdacf4
Mircea CALIN <miorita76@gmail.com>
2021-06-12 07:27:22 +0300

    Created two directories - CSS, IMAGES. Inserted avatar.png image in IMAGES directory. Created styles.css in CSS directory. Commited PUBLIC directory from Andrian last lesson.

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title>Curriculum Vitae CALIN Mircea</title>
+</head>
+
+<body>
+
+   <h1>Mircea CALIN</h1>
+   <p>Email: miorita76@gmail.com</p>
+
+   <div>
+        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+    </div>
+
+</body>
+</html>
